7cca67b03f50616a981031adedf80e8d745a107c
olarif <76481740+olarif@users.noreply.github.com>
2022-07-08 14:20:11 +0200

    prefab

--- BA4Experimental/Assets/Scripts/Health.cs
+++ BA4Experimental/Assets/Scripts/Health.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public float health=100;
+    public float health = 100;
     public float hitDamage = .5f;
     float count;
-    bool isHit=false;
+    bool isHit = false;
     public float timeDifferenceForHitCount = .5f;
+
+    GameMaster gm;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
 
     void Update()
     {
@@ -20,28 +32,16 @@ public class Health : MonoBehaviour
 
         if(health <= 0)
         {
-            Debug.Log(this.gameObject.name + " died");
-            this.gameObject.SetActive(false);
-        }
-    }
-/*
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (!isHit)
-        {
-            if (collision.gameObject.tag == "BlackSub")
+            if (this.gameObject.CompareTag("Player"))
             {
-                health -= hitDamage;
-                isHit = true;
-                count = timeDifferenceForHitCount;
+                this.transform.position = gm.lastCheckpointPos;
+                health = 100;
             }
         }
     }
-*/
+
     private void OnParticleCollision(GameObject other)
     {
-        Debug.Log(this.gameObject.name);
-
         if (!isHit)
         {
                 health -= hitDamage;
